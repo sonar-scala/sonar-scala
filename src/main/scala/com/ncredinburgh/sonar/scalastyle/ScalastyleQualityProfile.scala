f35f007d8f58a25fc4580822e2666e4200ffdabf
Michael Zinsmaier <michael.zinsmaier@googlemail.com>
2015-10-11 09:55:10 +0200

    got it working with new simple key that also gets really rid of the key=class thing

--- src/main/scala/com/ncredinburgh/sonar/scalastyle/ScalastyleQualityProfile.scala
+++ src/main/scala/com/ncredinburgh/sonar/scalastyle/ScalastyleQualityProfile.scala
@@ -19,13 +19,14 @@
 package com.ncredinburgh.sonar.scalastyle
 
 import org.slf4j.LoggerFactory
-import org.sonar.api.profiles.{ProfileDefinition, RulesProfile}
-import org.sonar.api.rules.{RuleFinder, ActiveRule}
+import org.sonar.api.profiles.{ ProfileDefinition, RulesProfile }
+import org.sonar.api.rules.{ RuleFinder, ActiveRule }
 import org.sonar.api.utils.ValidationMessages
 import org.scalastyle.ScalastyleError
 import scala.xml.XML
+import collection.JavaConversions._
 import org.sonar.api.rules.RuleQuery
-
+import org.sonar.api.rules.Rule
 
 /**
  * This class creates the default "Scalastyle" quality profile from Scalastyle's default_config.xml
@@ -40,31 +41,49 @@ class ScalastyleQualityProfile(ruleFinder: RuleFinder) extends ProfileDefinition
     val enabledRules = defaultConfigRules filter (x => (x \ "@enabled").text.equals("true"))
     val defaultRuleClasses = enabledRules map (x => (x \ "@class").text)
 
-    for {clazz <- defaultRuleClasses} {
-      val ruleKey = ScalastyleRepository.getStandardKey(clazz)
-      val ruleOption = Option(ruleFinder.findByKey(Constants.RepositoryKey, ruleKey))
+    val query = RuleQuery.create().withRepositoryKey(Constants.RepositoryKey)
+    val repoRules = ruleFinder.findAll(query)
 
-       ruleOption match {
-          case None => validation.addWarningText(s"Rule $ruleKey not found in ${Constants.RepositoryKey} repository! Rule won't be activated.")
-          case Some(rule) => {
-            if (!rule.isTemplate()) {
-              val activated = profile.activateRule(rule, rule.getSeverity)
-              setParameters(activated)
-            }
+    for { clazz <- defaultRuleClasses } {
+      val ruleOption = repoRules.find(clazzMatch(_, clazz))
+
+      ruleOption match {
+        case None => validation.addWarningText(s"Rule for $clazz not found in ${Constants.RepositoryKey} repository! Rule won't be activated.")
+        case Some(rule) => {
+          if (!rule.isTemplate()) {
+            val activated = profile.activateRule(rule, rule.getSeverity)
+            setParameters(activated, clazz)
           }
         }
+      }
     }
 
     profile
   }
 
-  def setParameters(activeRule: ActiveRule) {
-    defaultConfigRules.find(x => (x \ "@class").text.equals(activeRule.getRuleKey) ) match {
+  def setParameters(activeRule: ActiveRule, clazz: String) {
+    // set parameters
+    defaultConfigRules.find(x => (x \ "@class").text.equals(clazz)) match {
       case Some(rule) => {
-        val params = (rule \ "parameters" \ "parameter").map(n => ((n \ "@name").text, n.text )).toMap
+        val params = (rule \ "parameters" \ "parameter").map(n => ((n \ "@name").text, n.text)).toMap
         params foreach { case (key, value) => activeRule.setParameter(key, value) }
       }
       case _ => log.warn("Default rule with key " + activeRule.getRuleKey + " could not found in default_config.xml")
+    }
+    
+    // set synthetic parameter
+    activeRule.setParameter(Constants.ClazzParam, clazz)
+  }
+
+  private def clazzMatch(rule: Rule, clazz: String): Boolean = {
+    Option(rule.getParam(Constants.ClazzParam)) match {
+      case Some(param) => {
+        param.getDefaultValue.equals(clazz)
+      }         
+      case None => {
+        log.warn(s"Could not find required parameter ${Constants.ClazzParam}, rule for $clazz cannot be activated")
+        false
+      }
     }
   }
 

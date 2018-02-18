f119c0033c60d77e2472aff6b31c64efeb5af40a
Matteo Gazzetta <matteo_gazzetta@hotmail.it>
2018-02-18 19:21:28 +0100

    Added sonar.scala.source version property and removed some sonar api deprecation usage

--- src/test/scala/com/sagacify/sonar/scala/ScalaPluginSpec.scala
+++ src/test/scala/com/sagacify/sonar/scala/ScalaPluginSpec.scala
@@ -1,5 +1,5 @@
 /*
- * Sonar Scalastyle Plugin
+ * Sonar Scala Plugin
  * Copyright (C) 2014 All contributors
  *
  * This program is free software; you can redistribute it and/or
@@ -18,34 +18,49 @@
  */
 package com.sagacify.sonar.scala
 
+import com.buransky.plugins.scoverage.measure.ScalaMetrics
+import com.buransky.plugins.scoverage.sensor.ScoverageSensor
+import com.ncredinburgh.sonar.scalastyle.{ScalastyleQualityProfile, ScalastyleRepository, ScalastyleSensor}
 import org.scalatest.{FlatSpec, Matchers}
+import org.sonar.api.utils.Version
 
 /**
- * Tests ScalastylePlugin
- */
-class ScalastylePluginSpec extends FlatSpec with Matchers {
+  * Tests ScalaPlugin
+  */
+class ScalaPluginSpec extends FlatSpec with Matchers {
 
-  val testee = new ScalaPlugin
+  import org.sonar.api.{Plugin, SonarQubeSide, SonarRuntime}
+  import org.sonar.api.internal.SonarRuntimeImpl
 
-  // TODO provide a context and check that after calling define()
+  val runtime: SonarRuntime = SonarRuntimeImpl.forSonarQube(Version.create(6, 7), SonarQubeSide.SCANNER)
+  val context = new Plugin.Context(runtime)
+  new ScalaPlugin().define(context)
 
-//  "a scala plugin" should "provide a scala sensor" in {
-//    assert(testee.getExtensions.contains(classOf[ScalaSensor]))
-//  }
-//
-//  it should "provide a scalastyle sensor" in {
-//    assert(testee.getExtensions.contains(classOf[ScalastyleSensor]))
-//  }
-//
-//  it should "provide a scalastyle repository" in {
-//    assert(testee.getExtensions.contains(classOf[ScalastyleRepository]))
-//  }
-//
-//  it should "provide a scala language" in {
-//    assert(testee.getExtensions.contains(classOf[Scala]))
-//  }
-//
-//  it should "provide a scalastyle quality profile" in {
-//    assert(testee.getExtensions.contains(classOf[ScalastyleQualityProfile]))
-//  }
+  "a scala plugin" should "provide a scala sensor" in {
+    assert(context.getExtensions.contains(classOf[ScalaSensor]))
+  }
+
+  it should "provide a scalastyle sensor" in {
+    assert(context.getExtensions.contains(classOf[ScalastyleSensor]))
+  }
+
+  it should "provide a scalastyle repository" in {
+    assert(context.getExtensions.contains(classOf[ScalastyleRepository]))
+  }
+
+  it should "provide a scala language" in {
+    assert(context.getExtensions.contains(classOf[Scala]))
+  }
+
+  it should "provide a scalastyle quality profile" in {
+    assert(context.getExtensions.contains(classOf[ScalastyleQualityProfile]))
+  }
+
+  it should "provide a scoverage sensor" in {
+    assert(context.getExtensions.contains(classOf[ScoverageSensor]))
+  }
+
+  it should "provide scala metrics" in {
+    assert(context.getExtensions.contains(classOf[ScalaMetrics]))
+  }
 }

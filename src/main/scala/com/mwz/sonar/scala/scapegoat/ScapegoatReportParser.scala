3daa721dc4e00280690ee41a206f248635e85359
Luis Miguel Mejía Suárez <lmejias@psl.com.co>
2018-06-17 11:43:02 -0500

    Implementing the Scapegoat report parser

--- src/main/scala/com/mwz/sonar/scala/scapegoat/ScapegoatReportParser.scala
+++ src/main/scala/com/mwz/sonar/scala/scapegoat/ScapegoatReportParser.scala
@@ -1,16 +1,31 @@
 package com.mwz.sonar.scala.scapegoat
 
-import java.nio.file.{Path, Paths}
+import inspections.Level
+import java.nio.file.Path
+import scala.xml.XML
 
 /** Used to mock the scoverage report parser in tests */
 private[scapegoat] trait ScapegoatReportParserAPI {
 
   /** Parses the scapegoat xml report and returns all scapegoat warnings by filename */
-  def parse(scapegoatReportPath: Path): Map[Path, Seq[ScapegoatWarning]]
+  def parse(scapegoatReportPath: Path): Map[String, Seq[ScapegoatWarning]]
 }
 
 /** Scapegoat XML reports parser */
 private[scapegoat] trait ScapegoatReportParser extends ScapegoatReportParserAPI {
-  override final def parse(scapegoatReportPath: Path): Map[Path, Seq[ScapegoatWarning]] =
-    ???
+  override final def parse(scapegoatReportPath: Path): Map[String, Seq[ScapegoatWarning]] = {
+    val scapegoatXMLReport = XML.loadFile(scapegoatReportPath.toFile)
+
+    val scapegoatWarnings = for {
+      warning <- scapegoatXMLReport \\ "warning"
+      line = (warning \@ "line").toInt
+      text = warning \@ "text"
+      snippet = warning \@ "snippet"
+      level = Level.fromName(warning \@ "level")
+      file = (warning \@ "file").replace(".", "/") // scalastyle:ignore LiteralArguments
+      inspectionId = warning \@ "inspection"
+    } yield ScapegoatWarning(line, text, snippet, level, file, inspectionId)
+
+    scapegoatWarnings.groupBy(warning => warning.file)
+  }
 }
--- src/main/scala/com/mwz/sonar/scala/scapegoat/ScapegoatWarning.scala
+++ src/main/scala/com/mwz/sonar/scala/scapegoat/ScapegoatWarning.scala
@@ -1,14 +1,13 @@
 package com.mwz.sonar.scala.scapegoat
 
 import inspections.Level
-import java.nio.file.Path
 
 /** Represents a warning in the scapegoat report */
 private[scapegoat] final case class ScapegoatWarning(
-  line: Int,
-  text: String,
-  snippet: String,
-  level: Level,
-  file: Path,
-  inspectionId: String
+    line:         Int,
+    text:         String,
+    snippet:      String,
+    level:        Level,
+    file:         String,
+    inspectionId: String
 )

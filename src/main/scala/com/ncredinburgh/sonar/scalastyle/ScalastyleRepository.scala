f0830966ad400bb970391363b7faf1f53eb9b21b
Michael Wizner <mwz@users.noreply.github.com>
2018-08-31 00:12:01 +0100

    TODO

--- src/main/scala/com/ncredinburgh/sonar/scalastyle/ScalastyleRepository.scala
+++ src/main/scala/com/ncredinburgh/sonar/scalastyle/ScalastyleRepository.scala
@@ -78,7 +78,7 @@ final class ScalastyleRepository extends RulesDefinition {
         // if a rule has at least one real parameter make it a template
         rule.setTemplate(repoRule.params.nonEmpty)
 
-        // TODO: Set the status of those rules as deprecated3?
+        // TODO: Set the status of those rules as deprecated?
         // rule.setStatus(RuleStatus.DEPRECATED)
       }
     }

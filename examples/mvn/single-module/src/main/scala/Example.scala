9c9a675221cd66295c2a04080ffbe7e995268f6c
Luis Miguel Mejía Suárez <lmejias@psl.com.co>
2019-03-08 14:28:56 -0500

    Adding Scapegoat & Scalastyle issues to the maven single module example project

--- examples/mvn/single-module/src/main/scala/Example.scala
+++ examples/mvn/single-module/src/main/scala/Example.scala
@@ -1,3 +1,18 @@
 object Example {
   def sum(a: Int, b: Int) = a + b
+
+  // An example method full of horrible style issues!
+  // This is only indended for demonstration purposes.
+  // Please don't do this at home, because every time
+  // you write code like this, a cute kitten dies!
+  def ExampleIssues: Unit = {
+    val isEmpty = List(1).size == 0
+    val a = (14 / 1).toInt
+    val b = if (true) true else false
+    var TWO = 1 + 1
+    while (true) {
+      println("WOW!")
+    }
+    return ()
+  }
 }

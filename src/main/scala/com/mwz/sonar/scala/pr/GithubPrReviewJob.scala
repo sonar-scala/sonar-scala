7352e167d4dc327c2b043343b8e7e6f9c5c09efc
Michael Wizner <mwz@users.noreply.github.com>
2019-12-14 19:07:29 +0000

    Improve coverage of the github PR review module. (#316)

--- src/main/scala/com/mwz/sonar/scala/pr/GithubPrReviewJob.scala
+++ src/main/scala/com/mwz/sonar/scala/pr/GithubPrReviewJob.scala
@@ -120,7 +120,7 @@ final class GithubPrReviewJob(
       // Group patches by file names - `filename` is the full path relative to the root
       // of the project, so it should be unique. Raise an error when no files are present.
       prPatches <- Sync[F].fromEither(Either.fromOption(NonEmptyList.fromList(files), NoFilesInPR))
-      allPatches = prPatches.groupByNem(_.filename).map(_.head).toSortedMap.toMap
+      allPatches = prPatches.groupByNem(_.filename).map(_.head).toSortedMap
       // Filter out issues which aren't related to any files in the PR.
       issues = globalIssues.allIssues.filterKeys(f => allPatches.keySet.contains(f.toString))
       // Get new comments and post them.
@@ -132,7 +132,9 @@ final class GithubPrReviewJob(
       _ <- commentsToPost
         .sortBy(c => (c.path, c.position))
         .traverse { comment =>
-          Logger[F].debug(s"Posting a new comment $comment.") >>
+          Logger[F].debug(
+            s"Posting a new comment for ${comment.path}:${comment.position} - ${comment.body}"
+          ) >>
           github.createComment(comment)
         }
     } yield reviewStatus(issues)
--- src/test/scala/com/mwz/sonar/scala/fixtures.scala
+++ src/test/scala/com/mwz/sonar/scala/fixtures.scala
@@ -20,6 +20,10 @@ package com.mwz.sonar.scala
 import java.io.File
 import java.nio.file.{Files, Path}
 
+import cats.effect.IO
+import cats.effect.concurrent.Ref
+import com.mwz.sonar.scala.util.Logger
+
 trait WithFiles {
   def withFiles(paths: String*)(test: Seq[File] => Any): Unit = {
     val tmpDir: Path = Files.createTempDirectory("")
@@ -31,5 +35,32 @@ trait WithFiles {
       files.foreach(f => Files.deleteIfExists(f.toPath))
       Files.deleteIfExists(tmpDir)
     }
+  }
+}
+
+trait WithTracing {
+  def withTracing(test: Ref[IO, List[String]] => Any): Unit =
+    test(Ref.unsafe[IO, List[String]](List.empty))
+}
+
+trait WithLogging {
+  object LogLevel {
+    sealed trait Level
+    case object Debug extends Level
+    case object Info extends Level
+    case object Warn extends Level
+    case object Error extends Level
+  }
+
+  def withLogging(test: (Ref[IO, List[(LogLevel.Level, String)]], Logger[IO]) => Any): Unit = {
+    val logs = Ref.unsafe[IO, List[(LogLevel.Level, String)]](List.empty)
+    val logger: Logger[IO] = new Logger[IO] {
+      def debug(s: String): IO[Unit] = logs.update((LogLevel.Debug, s) :: _)
+      def info(s: String): IO[Unit] = logs.update((LogLevel.Info, s) :: _)
+      def warn(s: String): IO[Unit] = logs.update((LogLevel.Warn, s) :: _)
+      def error(s: String): IO[Unit] = logs.update((LogLevel.Error, s) :: _)
+      def error(s: String, e: Throwable): IO[Unit] = logs.update((LogLevel.Error, s) :: _)
+    }
+    test(logs, logger)
   }
 }

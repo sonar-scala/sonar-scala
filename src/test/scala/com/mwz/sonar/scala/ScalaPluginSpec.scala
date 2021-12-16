b744f4387f2c0f604f7610bd5ab867831fed55c0
Michael Wizner <mwz@users.noreply.github.com>
2021-12-16 00:25:34 +0000

    Update SonarQube to 8.9 LTS (#782)

--- src/test/scala/com/mwz/sonar/scala/ScalaPluginSpec.scala
+++ src/test/scala/com/mwz/sonar/scala/ScalaPluginSpec.scala
@@ -28,7 +28,7 @@ import org.sonar.api.utils.Version
 
 class ScalaPluginSpec extends AnyFlatSpec with Matchers {
   val runtime: SonarRuntime = SonarRuntimeImpl.forSonarQube(
-    Version.create(8, 7),
+    Version.create(8, 9),
     SonarQubeSide.SCANNER,
     SonarEdition.COMMUNITY
   )
